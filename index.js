d13800f27bfe92305560f7fe22e79bfd0a8a9f90
Ramón Huidobro <hola@ramonh.dev>
2022-02-19 12:35:58 +0100

    Fix typo that leads to crash in loading element

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Get ahold of the element with the ID "today-date"
-const dateElement = document.getElementByID("today-date");
+// https://developer.mozilla.org/es/docs/Web/API/Document/getElementById
+const dateElement = document.getElementById("today-date");
 
 // Set that element's HTML (inner HTML) to be a string representation of today's date
 // https://developer.mozilla.org/es/docs/Web/API/Element/innerHTML
